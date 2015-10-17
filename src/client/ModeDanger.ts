368af8f79a9de7ef4f3ceb59855fc9180381e044
Johannes Bader <jogus@live.de>
2015-10-17 23:48:10 +0200

    danger style overhaul

--- src/client/ModeDanger.ts
+++ src/client/ModeDanger.ts
@@ -8,6 +8,8 @@ import ModeTS = require("Mode");
 type Mode = ModeTS.Mode;
 var Mode = ModeTS.Mode;
 
+function saturate(x: number): number { return x < 0 ? 0 : (x > 1 ? 1 : x); }
+
 export class ModeDanger extends Mode
 {
 	private detectors: { description: string, dangerLevel: () => number }[] = [];
@@ -17,15 +19,15 @@ export class ModeDanger extends Mode
 		super(env);
 		this.detectors.push({
 			description: "suspicious acceleration",
-			dangerLevel: () => Math.random() * 3 | 0
+			dangerLevel: () => Math.random()
 		});
 		this.detectors.push({
 			description: "suspicious sounds",
-			dangerLevel: () => Math.random() * 3 | 0
+			dangerLevel: () => Math.random()
 		});
 		this.detectors.push({
 			description: "suspicious movement",
-			dangerLevel: () => Math.random() * 3 | 0
+			dangerLevel: () => Math.random()
 		});
 	}
 
@@ -44,7 +46,11 @@ export class ModeDanger extends Mode
 	{
 		this.detectors.forEach((detector, i) =>
 		{
-			$("p.detectorPanel").eq(i).removeClass("detectorPanel0 detectorPanel1 detectorPanel2").addClass("detectorPanel" + detector.dangerLevel());
+			var level = detector.dangerLevel();
+			var x = $("p.detectorPanel").eq(i);
+			x
+				.css("background-color", "rgb(" + (saturate(level * 2) * 85 | 0) + ", " + (saturate(2 - level * 2) * 85 | 0) + ", 0)")
+				.css("line-height", x.height() + "px");
 		});
 	}
 
