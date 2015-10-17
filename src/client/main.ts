e6398c0dc2b325c3927302d417f9944ea48e8d56
Johannes Bader <jogus@live.de>
2015-10-17 12:39:15 +0200

    empty project

---
+++ src/client/main.ts
@@ -0,0 +1,11 @@
+/// <reference path="../decls/jquery.d.ts" />
+/// <reference path="../decls/require.d.ts" />
+/// <reference path="../decls/socket.io-client.d.ts" />
+/// <reference path="../shared/include.ts" />
+/// <reference path="../decls/webrtc/MediaStream.d.ts" />
+
+import $ = require("jquery");
+
+$(() => {
+    console.log("Hello World");
+});
---
+++ src/server/main.ts
@@ -0,0 +1,25 @@
+/// <reference path="../decls/node.d.ts" />
+/// <reference path="../decls/express.d.ts" />
+/// <reference path="../shared/include.ts" />
+
+// config
+var confAppIp: string = "localhost";
+var confAppPort: number = 8080;
+
+import express = require("express");
+
+console.log("");
+console.log("*********************");
+console.log("*                   *");
+console.log("*   JACOBS server   *");
+console.log("*                   *");
+console.log("*********************");
+console.log("");
+
+// APP
+var app = express();
+var server = require("http").createServer(app);
+
+app.use("/", express.static("build/client"));
+
+server.listen(confAppPort);
