359873912e0314d51d6fb0130c107fbf0fc71519
amtrix <amtrix92@gmail.com>
2015-10-18 07:37:25 +0200

    More game stuff

--- src/client/ModeGame.ts
+++ src/client/ModeGame.ts
@@ -12,13 +12,16 @@ export class ModeGame extends Mode
 {
     private context: CanvasRenderingContext2D;
 
-    private currY: number = 0;
-    private nextBoxY: number = 100;
-    private nextBoxDx: number = 1;
-    private myDx: number = 1;
-    private yRate: number = 1;
-    private frameCount: number = 0;
+    private currY: number;
+    private nextBoxY: number;
+    private nextBoxDx: number;
+    private myDx: number;
+    private yRate: number;
+    private frameCount: number;
     private song: HTMLAudioElement;
+    private dead: boolean;
+    private container: JQuery;
+    private points: number;
 
     public constructor(env: Environment)
     {
@@ -32,18 +35,21 @@ export class ModeGame extends Mode
     
     public init(container: JQuery): void
     {
+        this.points = 0;
+        this.container = container;
         this.currY = 0;
         this.nextBoxY = 100;
         this.nextBoxDx = this.getRandTrack();
         this.myDx = 1;
-        this.yRate = 1;
+        this.yRate = 0.1;
         this.frameCount = 0;
         this.song = (<HTMLAudioElement>$("#gameSong")[0]);
+        this.dead = false;
 
         this.song.loop = true;
         this.song.play();
 
-        container.on("mousedown touchstart", eo => {
+        container.on("touchend", eo => {
             var startX = eo.pageX || (<any>eo.originalEvent).changedTouches[0].pageX;
             startX -= container.offset().left;
 
@@ -65,28 +71,34 @@ export class ModeGame extends Mode
     {
         this.frameCount++;
         this.currY += this.yRate;
-        if (this.frameCount % 500)
-            this.yRate += 0.1;
+        if (this.frameCount % 500 == 0)
+            this.yRate += 0.01;
 
         var boxDst = this.nextBoxY - this.currY;
 
         if (this.myDx == this.nextBoxDx) {
             if (boxDst < 0) { // HIT! -> gameover
-                while(1){};
+                if (!this.dead) {
+                    alert("Game over: " + this.points);
+                    this.container.children().remove();
+                    this.init(this.container);
+                    this.dead = true;
+                }
             } else {
-                (<HTMLAudioElement>$("#gameSong")[0]).volume = Math.min(boxDst / 100, 1);
+                this.song.volume = Math.min(boxDst / 100, 1);
             }
         } else {
             if (boxDst < 0) {
+                this.points++;
                 this.nextBoxDx = this.getRandTrack();
                 this.nextBoxY = this.currY + 100 * (0.1 + Math.random());
             }
-            (<HTMLAudioElement>$("#gameSong")[0]).volume = 1;
+            this.song.volume = 1;
         }
     }
-    
-    public processImage(imageData: ImageData)
+
+    public getTitle(): string
     {
-        throw "VIRTUAL";
+        return "game"
     }
 }
